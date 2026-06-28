b57680ec8fa5253cf1b76c3f1b788e55d946a8df
arunkumarmudakavi <arunkumarmudakavi029@gmail.com>
2026-06-28 16:06:02 +0100

    feat: Refactor PlannerAgent to generate tasks dynamically from planner(llm) response

--- backend/agents/planner_agent.py
+++ backend/agents/planner_agent.py
@@ -1,31 +1,41 @@
+import json
+import time
+
 from backend.agents.state import ResearchState, Task
+from backend.llm.prompts.planner_prompt import PLANNER_PROMPT
 
 
 class PlannerAgent:
+    def __init__(self, llm_client):
+        self.llm_client = llm_client
+
     def execute(self, state: ResearchState) -> ResearchState:
         if state.status != "PENDING":
             return state
 
-        query = state.query
-        state.tasks = [
-            Task(
-                topic="Overview",
-                description=f"Research overview of {query}"
-            ),
-            Task(
-                topic="Benefits",
-                description=f"Research benefits of {query}"
-            ),
-            Task(
-                topic="Challenges",
-                description=f"Research challenges of {query}"
-            ),
-            Task(
-                topic="Future Trends",
-                description=f"Research future trends of {query}"
-            ),
-        ]
+        prompt = PLANNER_PROMPT.format(query=state.query)
 
+        # start = time.time()
+        response = self.llm_client.generate(prompt)
+        # end = time.time()
+
+        # print(f"Time taken {end - start:.2f} seconds")
+        # print("res: ", response)
+
+        try:
+            topics = json.loads(response)
+        except json.JSONDecodeError:
+            state.errors.append("Failed to parse planner response.")
+            return state
+
+        tasks = []
+
+        for topic in topics:
+            tasks.append(
+                Task(topic=topic, description=f"Research {topic} of {state.query}")
+            )
+
+        state.tasks = tasks
         state.status = "PLANNING_COMPLETED"
 
         return state
